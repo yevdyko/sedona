6da3626bacd03db47aadc3e2d5c2234e8c09a857
Yev Dyko <evgeny.dyko@gmail.com>
2016-04-06 20:04:15 +0300

    Add .hamburger to all pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// google maps
+
 function initMap() {
   var mapDiv = document.getElementById('search__map');
   var myLatLng = {lat: 34.9032483, lng: -111.7539813};
@@ -13,5 +15,24 @@ function initMap() {
     position: myLatLng,
     map: map,
     icon: image
+  });
+}
+
+// hamburger
+
+var nojs = document.querySelector(".no-js");
+var menu = document.querySelector(".menu");
+var hamburger = document.querySelector(".hamburger");
+var close = document.querySelector(".is-active");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  nojs.classList.remove("no-js");
+});
+
+if (hamburger != null) {
+  hamburger.addEventListener("click", function(event) {
+    event.preventDefault();
+    menu.classList.toggle("menu--show");
+    hamburger.classList.toggle("is-active");
   });
 }
