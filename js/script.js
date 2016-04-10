9677d9e9c0297b21c253114a1c8122c0f5d15029
Yev Dyko <evgeny.dyko@gmail.com>
2016-04-10 20:44:08 +0300

    Replace all double quotes with single quotes in script.js file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-"use strict";
+'use strict';
 
 // google maps
-
 function initMap() {
-  var mapDiv = document.getElementById("search__map");
+  var mapDiv = document.getElementById('search__map');
   var myLatLng = {lat: 34.9032483, lng: -111.7539813};
   var map = new google.maps.Map(mapDiv, {
     center: myLatLng,
     scrollwheel: false,
     zoom: 7
   });
-  var image = "img/icons/icon-map-marker.svg";
+  var image = 'img/icons/icon-map-marker.svg';
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
@@ -19,20 +18,19 @@ function initMap() {
 }
 
 // hamburger
+var nojs = document.querySelector('.no-js');
+var menu = document.querySelector('.menu');
+var hamburger = document.querySelector('.hamburger');
+var close = document.querySelector('.is-active');
 
-var nojs = document.querySelector(".no-js");
-var menu = document.querySelector(".menu");
-var hamburger = document.querySelector(".hamburger");
-var close = document.querySelector(".is-active");
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  nojs.classList.remove("no-js");
+document.addEventListener('DOMContentLoaded', function(event) {
+  nojs.classList.remove('no-js');
 });
 
 if (hamburger != null) {
-  hamburger.addEventListener("click", function(event) {
+  hamburger.addEventListener('click', function(event) {
     event.preventDefault();
-    menu.classList.toggle("menu--show");
-    hamburger.classList.toggle("is-active");
+    menu.classList.toggle('menu--show');
+    hamburger.classList.toggle('is-active');
   });
 }
